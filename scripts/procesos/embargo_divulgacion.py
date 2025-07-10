9f8e23c9bbd6e9d7557aec835013c7eb5ee11e1e
IA. MechMind <ia.mechmind@gmail.com>
2025-07-10 15:15:11 +0200

    Create embargo_divulgacion.py

---
+++ scripts/procesos/embargo_divulgacion.py
@@ -0,0 +1,11 @@
+### Compensación
+bounty_program:
+  critical: 
+    reward: 10,000-50,000 QTC (Quantum Tokens)
+    conditions: 
+      - proof_of_concept_required: true
+      - affects_core_system: true
+  high: 
+    reward: 5,000-10,000 QTC
+  medium: 
+    reward: 1,000-5,000 QTC
